1a371376b06be2caa30368993485abbc54158d0a
Manuel Enrique Reyes Rivero <manuelreyes60@gmail.com>
2021-05-28 16:25:26 +0200

    DependencyInversion and InterfaceSegregation added

---
+++ DependencyInversionPrinciple/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DependencyInversionPrinciple
+{
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public class Person
+    {
+        public string Name;
+    }
+
+    public interface IReleationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+
+    public class Relationships : IReleationshipBrowser
+    {
+        private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
+        public IEnumerable<Person> FindAllChildrenOf(string name)
+        {
+            return relations.Where(x => x.Item1.Name == name && x.Item2 == Relationship.Parent).Select(r => r.Item3);
+        }
+
+        public List<(Person, Relationship, Person)> Relations => relations;
+    }
+
+    public class Research
+    {
+        // Better
+        public Research(IReleationshipBrowser browser)
+        {
+            foreach(var p in browser.FindAllChildrenOf("Manuel"))
+            {
+                Console.WriteLine($"Manuel has a child named {p.Name}");
+            }
+        }
+
+        // BAD
+/*        public Research(Relationships relationships)
+        {
+            var relations = relationships.Relations;
+            foreach (var r in relations.Where(x => x.Item1.Name == "Manuel" && x.Item2 == Relationship.Parent))
+            {
+                Console.WriteLine($"Manuel has a child named {r.Item3.Name}");
+            }
+        }*/
+    }
+
+    class Program
+    {
+        public string Name;
+        static void Main(string[] args)
+        {
+            var parent = new Person { Name = "Manuel" };
+            var child1 = new Person { Name = "Chris" };
+            var child2 = new Person { Name = "Mary" };
+
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
+
+            new Research(relationships);
+        }
+    }
+}
---
+++ InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace InterfaceSegregationPrinciple
+{
+    public class Document
+    {
+        public Document() { }
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            Console.WriteLine("Printing...");
+        }
+
+        public void Scan(Document d)
+        {
+            Console.WriteLine("Scanning...");
+        }
+    }
+
+    public class MultiFunctionDevice : IMultiFunctionDevice
+    {
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionDevice(IPrinter printer, IScanner scanner)
+        {
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        }
+    }
+
+    public interface IMultiFunctionDevice: IScanner, IPrinter { }
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public interface IFax
+    {
+        void Fax(Document d);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Photocopier p = new Photocopier();
+            p.Scan(new Document());
+            p.Print(new Document());
+
+            MultiFunctionDevice device = new MultiFunctionDevice(p, p);
+            device.Print(new Document());
+            device.Scan(new Document());
+        }
+    }
+}
