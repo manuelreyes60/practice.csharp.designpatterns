6b35764fa868d48600a6a01332a387a8eefc07e6
reyes <manuelreyes60@gmail.com>
2021-06-18 15:00:15 +0200

    Composite added

---
+++ Composite/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Collections.ObjectModel;
+using static System.Console;
+
+namespace Composite
+{
+    public class GraphicObject
+    {
+        public virtual string Name { get; set; } = "Group";
+        public string Color;
+        private Lazy<List<GraphicObject>> children = new Lazy<List<GraphicObject>>();
+        public List<GraphicObject> Children => children.Value;
+
+        private void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('*', depth))
+              .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color} ")
+              .AppendLine($"{Name}");
+            foreach (var child in Children)
+                child.Print(sb, depth + 1);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+    }
+
+    public class Circle : GraphicObject
+    {
+        public override string Name => "Circle";
+    }
+
+    public class Square : GraphicObject
+    {
+        public override string Name => "Square";
+    }
+
+    public static class ExtensionMethods
+    {
+        public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if (ReferenceEquals(self, other)) return;
+
+            foreach (var from in self)
+                foreach (var to in other)
+                {
+                    from.Out.Add(to);
+                    to.In.Add(from);
+                }
+        }
+    }
+
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float Value;
+        public List<Neuron> In = new List<Neuron>();
+        public List<Neuron> Out = new List<Neuron>();
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield return this;
+        }
+    }
+
+    public class NeuronLayer : Collection<Neuron>
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drawing = new GraphicObject { Name = "My Drawing" };
+            drawing.Children.Add(new Square { Color = "Red" });
+            drawing.Children.Add(new Circle { Color = "Yellow" });
+
+            var group = new GraphicObject();
+            group.Children.Add(new Circle { Color = "Blue" });
+            group.Children.Add(new Square { Color = "Blue" });
+            drawing.Children.Add(group);
+
+            WriteLine(drawing);
+
+            var neuron1 = new Neuron();
+            var neuron2 = new Neuron();
+            var layer1 = new NeuronLayer();
+            var layer2 = new NeuronLayer();
+
+            neuron1.ConnectTo(neuron2);
+            neuron1.ConnectTo(layer1);
+            layer1.ConnectTo(layer2);
+        }
+    }
+}
