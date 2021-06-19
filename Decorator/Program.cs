16e02d77c38a76d47a1d15650e6577f5c263ff7b
reyes <manuelreyes60@gmail.com>
2021-06-19 16:10:24 +0200

    decorator added

---
+++ Decorator/Program.cs
@@ -0,0 +1,193 @@
+﻿using Autofac;
+using System;
+
+namespace Decorator
+{
+    public interface IBird
+    {
+        void Fly();
+    }
+
+    public interface ILizard
+    {
+        void Crawl();
+    }
+
+    public class Bird : IBird
+    {
+        public int Weight { get; set; }
+        public void Fly()
+        {
+            Console.WriteLine($"Flying in the sky with weight {Weight}");
+        }
+    }
+
+    public class Lizard : ILizard
+    {
+        public int Weight { get; set; }
+        public void Crawl()
+        {
+            Console.WriteLine($"Crawling in the dirt with weight {Weight}");
+        }
+    }
+    public class Dragon : IBird, ILizard
+    {
+        private Bird bird = new Bird();
+        private Lizard lizard = new Lizard();
+        private int weight;
+
+        public void Crawl()
+        {
+            lizard.Crawl();
+        }
+
+        public void Fly()
+        {
+            bird.Fly();
+        }
+
+        public int Weight { 
+            get => weight; 
+            set
+            {
+                weight = value;
+                bird.Weight = value;
+                lizard.Weight = value;
+            } 
+        }
+    }
+    public interface IShape
+    {
+        string AsString();
+    }
+    public class Circle : IShape
+    {
+        private float radius;
+        public Circle(float radius)
+        {
+            this.radius = radius;
+        }
+
+        public string AsString()
+        {
+            return $"Circle with radius {radius}";
+        }
+
+        public void Resize(float factor)
+        {
+            radius *= factor;
+        }
+    }
+    public class Square : IShape
+    {
+        private float side;
+        public Square(float side)
+        {
+            this.side = side;
+        }
+
+        public string AsString()
+        {
+            return $"Square with side {side}";
+        }
+    }
+    public class ColoredShape : IShape
+    {
+        private IShape shape;
+        private string color;
+
+        public ColoredShape(IShape shape, string color)
+        {
+            this.shape = shape;
+            this.color = color;
+        }
+
+        public string AsString()
+        {
+            return $"{shape.AsString()} has the color {color}";
+        }
+    }
+    public class TransparentShape : IShape
+    {
+        private IShape shape;
+        private float transparency;
+
+        public TransparentShape(IShape shape, float transparency)
+        {
+            this.shape = shape;
+            this.transparency = transparency;
+        }
+
+        public string AsString()
+        {
+            return $"{shape.AsString()} has {transparency * 100.0}% transparency";
+        }
+    }
+    public interface IReportingService
+    {
+        void Report();
+    }
+
+    public class ReportingService : IReportingService
+    {
+        public void Report()
+        {
+            Console.WriteLine("Here is your report");
+        }
+    }
+
+    public class ReportingServiceWithLogging : IReportingService
+    {
+        private IReportingService decorated;
+
+        public ReportingServiceWithLogging(IReportingService decorated)
+        {
+            if (decorated == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(decorated));
+            }
+            this.decorated = decorated;
+        }
+
+        public void Report()
+        {
+            Console.WriteLine("Commencing log...");
+            decorated.Report();
+            Console.WriteLine("Ending log...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Dragon();
+            d.Weight = 123;
+            d.Fly();
+            d.Crawl();
+
+            var square = new Square(1.23f);
+            Console.WriteLine(square.AsString());
+
+            var redSquare = new ColoredShape(square, "red");
+            Console.WriteLine(redSquare.AsString());
+
+            var redHalfTransparentSquare = new TransparentShape(redSquare, 0.5f);
+            Console.WriteLine(redHalfTransparentSquare.AsString());
+
+            var b = new ContainerBuilder();
+            b.RegisterType<ReportingService>().Named<IReportingService>("reporting");
+            b.RegisterDecorator<IReportingService>(
+                (context, service) => new ReportingServiceWithLogging(service),
+              "reporting");
+
+            // open generic decorators also supported
+            // b.RegisterGenericDecorator()
+
+            using (var c = b.Build())
+            {
+                var r = c.Resolve<IReportingService>();
+                r.Report();
+            }
+        }
+    }
+}
