324277b1ce137f42771243934cf00d384792f85d
reyes <manuelreyes60@gmail.com>
2021-06-23 10:40:03 +0200

    Null Object added

---
+++ NullObject/Program.cs
@@ -0,0 +1,85 @@
+﻿using Autofac;
+using System;
+using System.Dynamic;
+using static System.Console;
+
+namespace NullObject
+{
+    public interface ILog
+    {
+        void Info(string msg);
+        void Warn(string msg);
+    }
+
+    class ConsoleLog : ILog
+    {
+        public void Info(string msg)
+        {
+            WriteLine(msg);
+        }
+
+        public void Warn(string msg)
+        {
+            WriteLine("WARNING: " + msg);
+        }
+    }
+
+    public class BankAccount
+    {
+        private ILog log;
+        private int balance;
+
+        public BankAccount(ILog log)
+        {
+            this.log = log;
+        }
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            // check for null everywhere
+            log?.Info($"Deposited ${amount}, balance is now {balance}");
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                log?.Info($"Withdrew ${amount}, we have ${balance} left");
+            }
+            else
+            {
+                log?.Warn($"Could not withdraw ${amount} because balance is only ${balance}");
+            }
+        }
+    }
+
+    public sealed class NullLog : ILog
+    {
+        public void Info(string msg)
+        {
+
+        }
+
+        public void Warn(string msg)
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<BankAccount>();
+            cb.RegisterType<NullLog>().As<ILog>();
+            using(var c = cb.Build())
+            {
+                var ba = c.Resolve<BankAccount>();
+                ba.Deposit(100);
+            }
+        }
+    }
+}
